a73371b9ab88b3f9d67afbb7d8da0f28abab4519
Renan <machadosantosrenan@gmail.com>
2025-08-24 17:55:56 -0300

    Server: Done secret_key registration on proxy procedure

--- Server/src/main.rs
+++ Server/src/main.rs
@@ -1,3 +1,4 @@
+use std::fmt::format;
 use std::fs;
 use std::net::TcpListener;
 use std::net::TcpStream;
@@ -23,6 +24,58 @@ fn generate_secret_key_string(size: usize, charset: &[u8]) -> String {
     }).collect()
 }
 
+/// Print a custom pattern message on concole
+/// 
+/// # Arguments
+/// 
+/// * `message: String` - Message that will be printed out.
+fn report(message: String) -> () {
+    println!("[SERVER] :: {}", message);
+}
+
+/// Try to register a secret-key at proxy
+/// 
+/// # Arguments
+/// 
+/// * `secret: &str` - String reference that contains the secret-key.
+/// 
+/// ## Returns
+/// Nothing if the registration is successfull
+/// A String if any error occurr
+fn register_with_proxy(secret: &str) -> Result<(), String> {
+    match TcpStream::connect("0.0.0.0:2006") {
+        Ok(mut stream) => {
+            let request = format!(
+                "POST /register-secret HTTP/1.1\r\n\
+                Host: 0.0.0.0:2006\r\n\
+                Content-Type: text/plain\r\n\
+                Content-Length: {}\r\n\
+                \r\n\
+                {}",
+                secret.len(),
+                secret
+            );
+
+            stream.write(request.as_bytes()).unwrap();
+
+            let mut response_buffer = [0; 512];
+            stream.read(&mut response_buffer).map_err(|e|e.to_string());
+            let response_str = String::from_utf8_lossy(&response_buffer);
+
+            if response_str.starts_with("HTTP/1.1 200 OK") {
+                report(format!("Secret Key has been setted up with proxy."));
+                Ok(())
+            } else {
+                Err(format!("Secret Key registration have failed. Proxy's answer: {}", response_str))
+            }
+        },
+        Err(e) => Err(format!("Connection with proxy have failed!"))
+    }
+}
+
+fn handle_connection(mut stream: TcpStream, secret: Arc<String>) {
+    
+}
 
 fn main() {
     const SECRET_KEY_CHARSET: &[u8] = b"ABCDEFGIJKLMNOPQRTUVWXYZ\
@@ -30,11 +83,30 @@ fn main() {
                                         0123456789\
                                         !@#$%&*()-=[]{},.;?<>";
     
+    //Generates a random String
     let secret_key_string = generate_secret_key_string(16, SECRET_KEY_CHARSET);
     
+    //Made a SHA-256 key with random String data
     let mut hasher = Sha256::new();
     hasher.update(secret_key_string.as_bytes());
     let hash_as_bytes = hasher.finalize();
     let secret_key = hex::encode(hash_as_bytes);
+
+    report(format!("Secret Key Generated! >>> {}", &secret_key));
+
+    while let Err(e) = register_with_proxy(&secret_key.as_str()) {
+        eprintln!("[SERVER] :: Critical Error >> {}", e);
+    }
+
     let arc_secret_key = Arc::new(secret_key);
+
+    let listener =  TcpListener::bind("127.0.0.1:1445").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        let secret_key_clone = Arc::clone(&arc_secret_key);
+        thread::spawn(move || {
+            handle_connection(stream, secret_key_clone);
+        });
+    }
 }
